3a807cec60cb195381160a365ed9bbf1dc1be1d7
Hetlr88 <mrhetlr0@gmail.com>
2024-08-25 22:50:29 +0300

    Update __init__.py

--- bot/helper/__init__.py
+++ bot/helper/__init__.py
@@ -1,14 +1,6 @@
 from pyrogram import Client, filters
 
-from decouple import config
 
-api = 29452145
-hash = "5a2784e571fe5043852d32396a34a13b"
-bot = "7445547849:AAHfX4_8TQ57cg71Q_l9ABoa6RfgfPWHgsE"
-owner = str("6169288210").split()
-group = "-1002243837012"
-block = "1 2 3"
-try:
     api = config("api", cast=int)
     hash = config("hash")
     bot = config("bot")
